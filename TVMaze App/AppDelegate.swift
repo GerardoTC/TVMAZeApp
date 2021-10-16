2d631f756d9da6719f2d7f829170615514b51582
Gerardo Tarazona Caceres <getaca@gmail.com>
2021-10-16 07:56:01 -0500

    initial State

---
+++ TVMaze App/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  TVMaze App
+//
+//  Created by Gerardo Tarazona Caceres on 15/10/21.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
---
+++ TVMaze App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TVMaze App
+//
+//  Created by Gerardo Tarazona Caceres on 15/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
